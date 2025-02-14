103798adb6e8a20dab1496b431d1ebe97a44b0d1
D. MacCarthy <sc8pr.py@gmail.com>
2025-02-14 13:29:49 -0700

    Modify load.py

--- sal_cs/load.py
+++ sal_cs/load.py
@@ -2,11 +2,9 @@ from os.path import split
 
 _sample = split(__file__)[0] + "/sample.txt"
 
-def load(fileName=""):
-	if len(fileName) == 0:
-		fileName = _sample
+def load(fileName=_sample):
 	try:
-		with open(fileName) as f:
+		with open(fileName, encoding="UTF-8") as f:
 			txt = f.read()
 	except Exception as e:
 		txt = str(e)
